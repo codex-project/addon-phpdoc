4ca7f8c3558489456ce9ecc3a3a19439dcad8bec
Robin Radic <robin@radic.nl>
2016-12-11 07:33:15 +0100

    upd

--- resources/views/document.blade.php
+++ resources/views/document.blade.php
@@ -1,35 +1,7 @@
-@extends(codex()->view('layout'))
+@extends(codex()->view('document'))
 
+@section('appClass', 'page-phpdoc')
 
-@section('body')
-
-    <!--[if lt IE 10]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
-<div id="app" class="page-phpdoc" v-cloak>
-    <c-theme :class="classes">
-        <c-header ref="header" :show-toggle="true" :logoLink="{ name: 'welcome' }">
-            <div slot="menu">
-                @stack('nav')
-            </div>
-        </c-header>
-
-        <div class="c-page" ref="page" :style="{ 'min-height': minHeights.page + 'px' }">
-            <c-sidebar ref="sidebar" :min-height="minHeights.inner" active="{{ $document->url() }}">
-                {!! $ref->getSidebarMenu()->render($document->getProject(), $document->getRef()) !!}
-            </c-sidebar>
-
-            <c-content ref="content" :min-height="minHeights.inner" autoloader-languages-path="{{ asset('vendor/codex') }}/vendor/prismjs/components/">
-                {!! $document->render()  !!}
-            </c-content>
-        </div>
-
-        <c-scroll-to-top></c-scroll-to-top>
-
-        <c-footer ref="footer">
-            <p>footer</p>
-        </c-footer>
-    </c-theme>
-</div>
-
+@section('content')
+    {!! $document->render()  !!}
 @stop

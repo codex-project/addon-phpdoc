f038a45693e904f8b6d3fc458cdbf11a79a707a4
Robin Radic <robin@radic.nl>
2016-12-27 08:55:26 +0100

    Header copyright update

--- src/Http/routes.api.php
+++ src/Http/routes.api.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Part of the Codex Project packages.
+ *
+ * License and copyright information bundled with this package in the LICENSE file.
+ *
+ * @author    Robin Radic
+ * @copyright Copyright 2016 (c) Codex Project
+ * @license   http://codex-project.ninja/license The MIT License
+ */
+
 //
 //Route::group([ 'prefix' => '{projectSlug}/{ref}' ], function () {
 //    Route::get('entity', [ 'as' => 'phpdoc', 'uses' => 'PhpdocApiController@getEntity' ]);
